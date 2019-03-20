3c71cec1d8d43c6bbc826dc5766ce6e670aeef26
online.tapu@gmail.com <online.tapu@gmail.com>
2019-03-21 02:47:48 +0600

    First Commit

---
+++ Java Spring Prctice Code/SpringFirst/src/com/tapumandal/FirstTask.java
@@ -0,0 +1,14 @@
+package com.tapumandal;
+
+public class FirstTask {
+	
+	private String message;
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void getMessage() {
+		System.out.println(this.message);
+	}
+}
